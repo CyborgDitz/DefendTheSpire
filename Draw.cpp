a2175739223fe378f57ed7e1a82e59f4bbc6c881
CyborgDitz <sebban.lol@gmail.com>
2025-03-11 11:30:05 +0100

    Initialzied Grid Project

---
+++ Draw.cpp
@@ -0,0 +1,27 @@
+#include "Draw.h"
+
+Tile grid[MAP_HEIGHT][MAP_WIDTH];
+void DrawGrid() {
+  for (int y = 0; y < MAP_HEIGHT; y++) {
+    for (int x = 0; x < MAP_WIDTH; x++) {
+      Rectangle tileRect = { (x * TILE_SIZE), (y * TILE_SIZE), TILE_SIZE, TILE_SIZE };
+
+      Color tileColor;
+      switch (grid[y][x].type) {
+        case EMPTY:
+          tileColor = RAYWHITE;
+        break;
+        case TOWER:
+          tileColor = BLUE;
+        break;
+        default:
+          tileColor = RAYWHITE;
+        break;
+      }
+
+      DrawRectangle(tileRect.x, tileRect.y, tileRect.width, tileRect.height, tileColor);
+
+      DrawRectangleLines(tileRect.x, tileRect.y, tileRect.width, tileRect.height, LIGHTGRAY);
+    }
+  }
+}
---
+++ globals.h
@@ -0,0 +1,31 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#define MAP_WIDTH 16
+#define MAP_HEIGHT 16
+#define TILE_SIZE 32.0f
+//todo tilemap 0 is empty, 1 is tower
+//todo 1 click on tile to change to 1
+
+//todo functions: draw tilemap, place tower(bool is occupied), get mouse click on tile
+struct Position
+ {
+    int x, y;
+ };
+struct Node
+ {
+  Position position;
+  };
+enum TileType
+ {
+  EMPTY = 0,
+  TOWER = 1
+  };
+
+struct Tile
+  {
+  TileType type;
+  Position position;
+  };
+
+#endif //GLOBALS_H
